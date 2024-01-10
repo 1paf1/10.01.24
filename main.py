9dc9616d853696abc41d155d0df938fa4736d592
Haisenberg <1paf1@ukr.net>
2024-01-10 17:29:36 +0200

    first task

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print("hello world")
+
+# 1.Користувач вводить рядок з клавіатури. Порахуйте кількість літер, цифр у рядку. Виведіть обидві кількості на екран.
+# (використовувати цикл)?
+'''letter_counter = 0
+digit_counter = 0
+row = input("Enter row: ")
+for letter in row:
+    if letter.isalpha():
+        letter_counter += 1
+    elif letter.isdigit():
+        digit_counter += 1
+print(f"Letters: {letter_counter}")
+print(f"Digits: {digit_counter}")'''
+
