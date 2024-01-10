723e8b80037f89581325b76d9b74aebdd5db48ff
Haisenberg <1paf1@ukr.net>
2024-01-10 17:25:50 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-# 2. Користувач вводить з клавіатури рядок та символ для пошуку. Порахуйте скільки разів у рядку зустрічається потрібний символ.
-# Отримане число виведіть на екран.
-total = 0
-row = input("Enter a text: ")
-symbol_to_find = input("Enter the symbol: ")
-for symbol in row:
-    if symbol == symbol_to_find:
-        total += 1
-print(total)
-
+print("hello world")
