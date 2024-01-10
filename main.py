25853d75e613274dbf0fed80ba70c1a38e9dadf7
Haisenberg <1paf1@ukr.net>
2024-01-10 17:43:39 +0200

    third task

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+# 3. Користувач вводить з клавіатури рядок, слово для пошуку, слово для заміни. Зробіть у рядку заміну одного слова на інше.
+# Виведіть триманий рядок на екрані.
+
+input_string = input("Enter string: ")
+word_to_find = input("Enter word to find: ")
+replacement_word = input("Enter word to replace: ")
+final_string = input_string.replace(word_to_find, replacement_word)
+
+print(f"final string: {final_string}")
+
